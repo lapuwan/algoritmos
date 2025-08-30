93bea7e6b85f52ba4d23742b7b4d4701546f893c
Pdro Csar <kit21pc@gmail.com>
2025-08-30 17:23:03 -0600

    Primer corrección de errores

--- tarea_1/hw_01_p1.py
+++ tarea_1/hw_01_p1.py
@@ -1,23 +1,32 @@
-list_01 = [5,5,6,23,4,345]
+# SOLUCIÓN CON DOS FOR ANIDADOS
+# En este código se permite que un valor sumado por sí mismo sea k
 
+# Arreglo
+list_01 = [5,7,23,4,345]
+
+# Valor de k
 k = 10
 is_true = False
-contador= 0
+
+# Variable del tamaño de la lista
 tamano_ls = len(list_01)
 
+print(f"Valor de k: {k}")
+
+# Separador (uso estético)
+print("--------------------------")
+
 for i in range(tamano_ls):
-    for j in range(i+1, tamano_ls):
+    for j in range(i, tamano_ls):
         if (list_01[i] + list_01[j] == k):
             is_true = True
-            print(f"{list_01[i]} + {list_01[j]} = {k}")
-            contador +=1
+            if(list_01[i] != list_01[j] ):
+                print(f"El primer par encontrado fue: {list_01[i]} + {list_01[j]} = {k}")
+            else:
+                print(f"El número {list_01[i]} sumado por sí mismo nos da {k}")
+            break
+    if(is_true):
+        break
 
-print("----------------------------")
-if (is_true == True):
-    print(f"Sí hay pares que den como suma {k}")
-    if (contador == 1):
-        print(f"En total hay sólo 1 par")
-    else:
-        print(f"En total hay {contador} pares")
-else:
-    print(f"No hay parejas que den como suma {k}")
+if(is_true == False):
+    print(f"No hay pares que den como suma {k}")
--- tarea_1/hw_01_p2.py
+++ tarea_1/hw_01_p2.py
@@ -1,23 +1,36 @@
-list_01 = [8,2,6,23,4,345,9,1]
+# SOLUCIÓN USANDO UN set() // Implementado como tabla hash
+# En este código se permite que un valor sumado por sí mismo sea k
 
+# La lista
+list_01 = [6,23,4,345,9,1]
+
+#El valor k
 k = 10
+
+# Uso de un set()
 set_ = set()
-contador = 0
+
+# Variable que nos permitirá imprimir en consola si no se encontró un par
 is_true = False
 
+print(f"Valor de k: {k}")
+
+# Separador (uso estético)
+print("--------------------------")
+
+# Recorremos la lista
 for i in list_01:
     complemento= k - i
     if (complemento in set_):
-        print(f"Si existe un par: {complemento} + {i}")
-        contador += 1
+        print(f"Si existe un par: {complemento} y {i}")
         is_true = True
+        break
+    elif(i == k/2):
+        print(f"El número {i} sumado por sí mismo nos da {k}")
+        is_true = True
+        break
     else:
         set_.add(i)
 
-print("--------------------------")
 if(is_true == False):
-    print(f"No hay pares que den como suma {k}")
-elif(contador == 1):
-    print(f"Hay sólo 1 par")
-else:
-    print(f"Hay {contador} pares")
+    print(f"No hay al menos un par que den como suma {k}")
